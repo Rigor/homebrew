66660a6ff04a7a40a603f312bec9ee52a56df7f4
Eric Tipton <eric.tipton@rigor.com>
2016-01-25 20:47:32 -0800

    Formulas needed so curl (+extensions) versions match prod's (Ubuntu apt package)

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,182 +1,84 @@
+require 'formula'
+
 class Openssl < Formula
-  desc "SSL/TLS cryptography library"
-  homepage "https://openssl.org/"
-  url "https://www.openssl.org/source/openssl-1.0.2e.tar.gz"
-  mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2e.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2e.tar.gz"
-  sha256 "e23ccafdb75cfcde782da0151731aa2185195ac745eea3846133f2e05c0e0bff"
-  revision 1
+  homepage 'http://openssl.org'
+  url 'https://www.openssl.org/source/openssl-1.0.1f.tar.gz'
+  mirror 'http://mirrors.ibiblio.org/openssl/source/openssl-1.0.1f.tar.gz'
+  sha256 '6cc2a80b17d64de6b7bac985745fdaba971d54ffd7d38d3556f998d7c0c9cb5a'
 
   bottle do
-    sha256 "ed8aab358427f6456f99b59dc04667fd5dada7a4bd60eb0a2ce40c20ed6c125c" => :el_capitan
-    sha256 "23a4c64aef59d211a4de12cd72c6109d96822065b1743f6db3cb45d623891989" => :yosemite
-    sha256 "a46133fb0a514504984083e34b3c65c9b770ce2ceb543e013e55be891ac68434" => :mavericks
+    sha1 "2687c0abb5e23d765bbd0024a010e36b05a8939e" => :mavericks
+    sha1 "dcaee2f1e51e8d0da7614e6dab4fc334f736d0de" => :mountain_lion
+    sha1 "4fabb39f5db46e8e62bf0b05e0133cd7e717860a" => :lion
   end
+
+  depends_on "makedepend" => :build if MacOS.prefer_64_bit?
 
   keg_only :provided_by_osx,
-    "Apple has deprecated use of OpenSSL in favor of its own TLS and crypto libraries"
-
-  option :universal
-  option "without-test", "Skip build-time tests (not recommended)"
-
-  deprecated_option "without-check" => "without-test"
-
-  depends_on "makedepend" => :build
-
-  # Xcode 7 Clang fix introduced regression which makes older Clang versions
-  # incorrectly declare suitable instructions.
-  # https://github.com/openssl/openssl/issues/494
-  # https://svn.macports.org/repository/macports/trunk/dports/devel/openssl/files/fix-Apple-clang-version-detection.patch
-  if MacOS.version <= :mountain_lion
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/patches/312f6228/openssl/fix-Apple-clang-version-detection.patch"
-      sha256 "a3e0e13e6c70d85d916bb88cbddc134952794d6292fbab4f2740ac9a07759606"
-    end
-  end
-
-  def arch_args
-    {
-      :x86_64 => %w[darwin64-x86_64-cc enable-ec_nistp_64_gcc_128],
-      :i386   => %w[darwin-i386-cc],
-    }
-  end
-
-  def configure_args; %W[
-    --prefix=#{prefix}
-    --openssldir=#{openssldir}
-    no-ssl2
-    zlib-dynamic
-    shared
-    enable-cms
-  ]
-  end
+    "The OpenSSL provided by OS X is too old for some software."
 
   def install
-    # Load zlib from an explicit path instead of relying on dyld's fallback
-    # path, which is empty in a SIP context. This patch will be unnecessary
-    # when we begin building openssl with no-comp to disable TLS compression.
-    # https://langui.sh/2015/11/27/sip-and-dlopen
-    inreplace "crypto/comp/c_zlib.c",
-              'zlib_dso = DSO_load(NULL, "z", NULL, 0);',
-              'zlib_dso = DSO_load(NULL, "/usr/lib/libz.dylib", NULL, DSO_FLAG_NO_NAME_TRANSLATION);'
+    args = %W[./Configure
+               --prefix=#{prefix}
+               --openssldir=#{openssldir}
+               zlib-dynamic
+               shared
+               enable-cms
+             ]
 
-    if build.universal?
-      ENV.permit_arch_flags
-      archs = Hardware::CPU.universal_archs
-    elsif MacOS.prefer_64_bit?
-      archs = [Hardware::CPU.arch_64_bit]
+    if MacOS.prefer_64_bit?
+      args << "darwin64-x86_64-cc" << "enable-ec_nistp_64_gcc_128"
     else
-      archs = [Hardware::CPU.arch_32_bit]
+      args << "darwin-i386-cc"
     end
 
-    dirs = []
+    system "perl", *args
 
-    archs.each do |arch|
-      if build.universal?
-        dir = "build-#{arch}"
-        dirs << dir
-        mkdir dir
-        mkdir "#{dir}/engines"
-        system "make", "clean"
-      end
-
-      ENV.deparallelize
-      system "perl", "./Configure", *(configure_args + arch_args[arch])
-      system "make", "depend"
-      system "make"
-
-      if (MacOS.prefer_64_bit? || arch == MacOS.preferred_arch) && build.with?("test")
-        system "make", "test"
-      end
-
-      if build.universal?
-        cp "include/openssl/opensslconf.h", dir
-        cp Dir["*.?.?.?.dylib", "*.a", "apps/openssl"], dir
-        cp Dir["engines/**/*.dylib"], "#{dir}/engines"
-      end
-    end
-
-    system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
-
-    if build.universal?
-      %w[libcrypto libssl].each do |libname|
-        system "lipo", "-create", "#{dirs.first}/#{libname}.1.0.0.dylib",
-                                  "#{dirs.last}/#{libname}.1.0.0.dylib",
-                       "-output", "#{lib}/#{libname}.1.0.0.dylib"
-        system "lipo", "-create", "#{dirs.first}/#{libname}.a",
-                                  "#{dirs.last}/#{libname}.a",
-                       "-output", "#{lib}/#{libname}.a"
-      end
-
-      Dir.glob("#{dirs.first}/engines/*.dylib") do |engine|
-        libname = File.basename(engine)
-        system "lipo", "-create", "#{dirs.first}/engines/#{libname}",
-                                  "#{dirs.last}/engines/#{libname}",
-                       "-output", "#{lib}/engines/#{libname}"
-      end
-
-      system "lipo", "-create", "#{dirs.first}/openssl",
-                                "#{dirs.last}/openssl",
-                     "-output", "#{bin}/openssl"
-
-      confs = archs.map do |arch|
-        <<-EOS.undent
-          #ifdef __#{arch}__
-          #{(buildpath/"build-#{arch}/opensslconf.h").read}
-          #endif
-          EOS
-      end
-      (include/"openssl/opensslconf.h").atomic_write confs.join("\n")
-    end
+    ENV.deparallelize
+    system "make", "depend" if MacOS.prefer_64_bit?
+    system "make"
+    system "make", "test"
+    #system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
+    system "make", "install_sw"
   end
 
   def openssldir
     etc/"openssl"
   end
 
-  def post_install
+  def cert_pem
+    openssldir/"cert.pem"
+  end
+
+  def osx_cert_pem
+    openssldir/"osx_cert.pem"
+  end
+
+  def write_pem_file
     keychains = %w[
       /Library/Keychains/System.keychain
       /System/Library/Keychains/SystemRootCertificates.keychain
     ]
 
-    certs_list = `security find-certificate -a -p #{keychains.join(" ")}`
-    certs = certs_list.scan(
-      /-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----/m
-    )
-
-    valid_certs = certs.select do |cert|
-      IO.popen("#{bin}/openssl x509 -inform pem -checkend 0 -noout", "w") do |openssl_io|
-        openssl_io.write(cert)
-        openssl_io.close_write
-      end
-
-      $?.success?
-    end
-
-    openssldir.mkpath
-    (openssldir/"cert.pem").atomic_write(valid_certs.join("\n"))
+    osx_cert_pem.atomic_write `security find-certificate -a -p #{keychains.join(" ")}`
   end
 
-  def caveats; <<-EOS.undent
-    A CA file has been bootstrapped using certificates from the system
-    keychain. To add additional certificates, place .pem files in
-      #{openssldir}/certs
+  def post_install
+    openssldir.mkpath
 
-    and run
-      #{opt_bin}/c_rehash
-    EOS
+    if cert_pem.exist?
+      write_pem_file
+    else
+      cert_pem.unlink if cert_pem.symlink?
+      write_pem_file
+      openssldir.install_symlink 'osx_cert.pem' => 'cert.pem'
+    end
   end
 
   test do
-    # Make sure the necessary .cnf file exists, otherwise OpenSSL gets moody.
-    assert (HOMEBREW_PREFIX/"etc/openssl/openssl.cnf").exist?,
-            "OpenSSL requires the .cnf file for some functionality"
-
-    # Check OpenSSL itself functions as expected.
-    (testpath/"testfile.txt").write("This is a test file")
-    expected_checksum = "e2d0fe1585a63ec6009c8016ff8dda8b17719a637405a4e23c0ff81339148249"
-    system "#{bin}/openssl", "dgst", "-sha256", "-out", "checksum.txt", "testfile.txt"
+    (testpath/'testfile.txt').write("This is a test file")
+    expected_checksum = "91b7b0b1e27bfbf7bc646946f35fa972c47c2d32"
+    system "#{bin}/openssl", 'dgst', '-sha1', '-out', 'checksum.txt', 'testfile.txt'
     open("checksum.txt") do |f|
       checksum = f.read(100).split("=").last.strip
       assert_equal checksum, expected_checksum
--- Library/Formula/rtmpdump.rb
+++ Library/Formula/rtmpdump.rb
@@ -1,45 +1,63 @@
-# Use a newer version instead of the upstream tarball:
-# http://livestreamer.tanuki.se/en/latest/issues.html#installed-rtmpdump-does-not-support-jtv-argument
+require 'formula'
+
 class Rtmpdump < Formula
-  desc "Tool for downloading RTMP streaming media"
-  homepage "https://rtmpdump.mplayerhq.hu"
-  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20150115.gita107cef.orig.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4%2b20150115.gita107cef.orig.tar.gz"
-  version "2.4+20150115"
-  sha256 "d47ef3a07815079bf73eb5d053001c4341407fcbebf39f34e6213c4b772cb29a"
+  homepage 'http://rtmpdump.mplayerhq.hu'
+  url 'http://rtmpdump.mplayerhq.hu/download/rtmpdump-2.3.tgz'
+  sha1 'b65ce7708ae79adb51d1f43dd0b6d987076d7c42'
 
-  head "git://git.ffmpeg.org/rtmpdump", :shallow => false
-
-  bottle do
-    cellar :any
-    revision 1
-    sha256 "ff54a87e4bf675b5b06942c73bfba5d8aad086f164fa90bdc2c53d74198072a5" => :el_capitan
-    sha256 "b6ff97c9829c5e4e49b907b0cda95586b32c2041408d97ba67a8eb3341da1425" => :yosemite
-    sha256 "b6a70460afac0e3a73643af0fdc61cd332e1aa48ec1f7adee20c280e4601e0a6" => :mavericks
-  end
-
-  conflicts_with "flvstreamer", :because => "both install 'rtmpsrv', 'rtmpsuck' and 'streams' binary"
+  head 'git://git.ffmpeg.org/rtmpdump'
 
   depends_on "openssl"
 
   fails_with :llvm do
-    build 2336
+    build '2336'
     cause "Crashes at runtime"
   end
 
+  # Use dylib instead of so
+  def patches; DATA; end unless build.head?
+
   def install
     ENV.deparallelize
+    sys_type = build.head? ? "darwin" : "posix"
     system "make", "CC=#{ENV.cc}",
                    "XCFLAGS=#{ENV.cflags}",
                    "XLDFLAGS=#{ENV.ldflags}",
                    "MANDIR=#{man}",
-                   "SYS=darwin",
+                   "SYS=#{sys_type}",
                    "prefix=#{prefix}",
-                   "sbindir=#{bin}",
                    "install"
   end
-
-  test do
-    system "#{bin}/rtmpdump", "-h"
-  end
 end
+
+__END__
+--- rtmpdump-2.3/librtmp/Makefile.orig	2010-07-30 23:05:25.000000000 +0200
++++ rtmpdump-2.3/librtmp/Makefile	2010-07-30 23:08:23.000000000 +0200
+@@ -25,7 +25,7 @@
+ CRYPTO_REQ=$(REQ_$(CRYPTO))
+ CRYPTO_DEF=$(DEF_$(CRYPTO))
+
+-SO_posix=so.0
++SO_posix=dylib
+ SO_mingw=dll
+ SO_EXT=$(SO_$(SYS))
+
+@@ -61,7 +61,7 @@
+ 	$(AR) rs $@ $?
+
+ librtmp.$(SO_EXT): $(OBJS)
+-	$(CC) -shared -Wl,-soname,$@ $(LDFLAGS) -o $@ $^ $> $(CRYPTO_LIB)
++	$(CC) -shared $(LDFLAGS) -o $@ $^ $> $(CRYPTO_LIB)
+ 	ln -sf $@ librtmp.so
+
+ log.o: log.c log.h Makefile
+@@ -87,5 +87,8 @@
+ 	cp librtmp.so.0 $(LIBDIR)
+ 	cd $(LIBDIR); ln -sf librtmp.so.0 librtmp.so
+
++install_dylib:	librtmp.dylib
++	cp librtmp.dylib $(LIBDIR)
++
+ install_dll:	librtmp.dll
+ 	cp librtmp.dll $(BINDIR)
+
